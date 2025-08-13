5698104a251e1126d3f6196f720fc5f5b859deb5
DelrosarioRovic <rovicdelrosario01@gmail.com>
2025-08-13 14:09:56 +0800

    fix

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-   console.log("hit contact api");
+   console.log({
+    host: process.env.SMTP_HOST
+   });
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
